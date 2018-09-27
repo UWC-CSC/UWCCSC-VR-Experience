12acda006492f8eeec70c4873872a00a09cafc4c
Marko Vejnovic <marko.vejnovic@hotmail.com>
2018-09-27 15:53:33 +0200

    Completely reworked everything. React is working now

--- frontend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Student, Person } from "./person";
-
-function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-let user = new Student("Jane", "M.", "User");
-
-document.body.innerHTML = greeter(user);
